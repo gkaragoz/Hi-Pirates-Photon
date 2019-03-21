ef0f3436fa580119dff1887ce186deaf0388e273
gkaragoz <gkaragoz96@gmail.com>
2019-03-21 21:56:44 +0300

    Create ShipMotor.cs

---
+++ Hi Pirates!/Assets/Scripts/ShipMotor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMotor : MonoBehaviour {
+
+    public bool goForward;
+    public bool rotatingRight, rotatingLeft;
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float slopeSpeed;
+
+    private float _rotationY, _rotationZ;
+
+    private void FixedUpdate() {
+        if (goForward)
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+        if (rotatingRight) {
+            _rotationY += Time.deltaTime * rotationSpeed;
+            if (_rotationZ > -15f)
+                _rotationZ -= Time.deltaTime * slopeSpeed;
+            transform.rotation = Quaternion.Euler(0, _rotationY, _rotationZ);
+        } else if (rotatingLeft) {
+            _rotationY -= Time.deltaTime * rotationSpeed;
+            if (_rotationZ < 15f)
+                _rotationZ += Time.deltaTime * slopeSpeed;
+            transform.rotation = Quaternion.Euler(0, _rotationY, _rotationZ);
+        } else {
+            if (_rotationZ > 0) {
+                _rotationZ -= Time.deltaTime * slopeSpeed;
+            } else if (_rotationZ < 0) {
+                _rotationZ += Time.deltaTime * slopeSpeed;
+            }
+            transform.rotation = Quaternion.Euler(0, _rotationY, _rotationZ);
+        }
+    }
+
+}
