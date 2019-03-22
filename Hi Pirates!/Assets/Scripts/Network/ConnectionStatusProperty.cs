e135ac0cfb3ee9a6c4e3feaa04820230b8e9633c
gkaragoz <gkaragoz96@gmail.com>
2019-03-22 21:54:07 +0300

    Create ConnectionStatusProperty.cs

---
+++ Hi Pirates!/Assets/Scripts/Network/ConnectionStatusProperty.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class ConnectionStatusProperty : MonoBehaviour {
+
+    private readonly string connectionStatusMessage = "Connection Status: ";
+
+    [SerializeField]
+    private TextMeshProUGUI _txtConnectionStatus;
+
+    private void Update() {
+        _txtConnectionStatus.text = connectionStatusMessage + PhotonNetwork.NetworkClientState;
+    }
+
+}
