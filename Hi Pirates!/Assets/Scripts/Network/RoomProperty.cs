baa33b3a8f40c9cd96914c1927419f3d4b4d08ea
gkaragoz <gkaragoz96@gmail.com>
2019-03-22 21:42:48 +0300

    Create RoomProperty.cs

---
+++ Hi Pirates!/Assets/Scripts/Network/RoomProperty.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class RoomProperty : MonoBehaviour {
+
+    [SerializeField]
+    private TextMeshProUGUI _txtCountOfRooms;
+    [SerializeField]
+    private TextMeshProUGUI _txtCountOfPlayers;
+
+    private int _cache = -1;
+
+    private void Update() {
+        if (PhotonNetwork.IsConnectedAndReady) {
+            _txtCountOfRooms.text = "Active Rooms:" + PhotonNetwork.CountOfRooms;
+            _txtCountOfPlayers.text = "Total Players: " + PhotonNetwork.CountOfPlayersOnMaster;
+        } else {
+            _txtCountOfRooms.text = "n/a";
+            _txtCountOfPlayers.text = "n/a";
+        }
+    }
+
+}
