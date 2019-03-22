b3da12a832c31376bbda276b7b5acb5fb110cbb1
gkaragoz <gkaragoz96@gmail.com>
2019-03-22 21:38:16 +0300

    Create ServerProperty.cs

---
+++ Hi Pirates!/Assets/Scripts/Network/ServerProperty.cs
@@ -0,0 +1,20 @@
+﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class ServerProperty : MonoBehaviour {
+
+    [SerializeField]
+    private TextMeshProUGUI _txtServerInfo;
+
+    private int _cache = -1;
+
+    private void Update() {
+        if (PhotonNetwork.IsConnectedAndReady) {
+            _txtServerInfo.text = "(" + PhotonNetwork.CloudRegion + ")" + PhotonNetwork.ServerAddress;
+        } else {
+            _txtServerInfo.text = "n/a";
+        }
+    }
+
+}
