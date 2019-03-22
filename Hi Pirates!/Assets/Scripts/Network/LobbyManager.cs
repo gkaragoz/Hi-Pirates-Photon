e56d70cfd6729710296ae12104067c0f242137e6
ManaShift <gurcaglarogulcan@gmail.com>
2019-03-22 19:48:20 +0300

    Matchmaking implementation

---
+++ Hi Pirates!/Assets/Scripts/Network/LobbyManager.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using Photon.Pun;
+using System.Collections.Generic;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    private Dictionary<int, GameObject> playerListEntries;
+
+    public string username;
+    // Start is called before the first frame update
+    void Start()
+    {
+        username = "Player " + Random.Range(1000, 10000);
+
+        PhotonNetwork.LocalPlayer.NickName = username;
+        PhotonNetwork.ConnectUsingSettings();
+
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+    }
+    void Awake()
+    {
+
+    }
+
+    #region PUN CALLBACKS
+
+    public override void OnConnectedToMaster()
+    {
+
+        Debug.Log("connected to master");
+        PhotonNetwork.JoinRandomRoom();
+      
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnJoinRoomFailed: " + message);
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnCreateRoomFailed: " + message);
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnJoinRandomFailed: " + message);
+        Debug.Log("Creating Room because no random room found");
+
+        string roomName = "Room " + Random.Range(1000, 10000);
+
+        RoomOptions options = new RoomOptions { MaxPlayers = 8 };
+
+        PhotonNetwork.CreateRoom(roomName, options, null);
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+        Debug.Log(cause);
+
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+
+    }
+
+    public override void OnLeftLobby()
+    {
+
+    }
+    public override void OnJoinedRoom()
+    {
+        if (playerListEntries == null)
+        {
+            playerListEntries = new Dictionary<int, GameObject>();
+        }
+
+        Debug.Log("joined room");
+    }
+    public override void OnLeftRoom()
+    {
+        Debug.Log("room left");
+
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+
+    }
+
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Hi Pirates!/Assets/Scripts/ShipController.cs
+++ Hi Pirates!/Assets/Scripts/ShipController.cs
@@ -1,15 +1,121 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using System.Collections;
+using UnityEngine;
 
 [RequireComponent(typeof(ShipMotor))]
-public class ShipController : MonoBehaviour {
+public class ShipController : MonoBehaviour
+{
 
     private ShipMotor _shipMotor;
+    private PhotonView _photonView;
 
-    private void Awake() {
+    private void Awake()
+    {
+        _photonView = GetComponent<PhotonView>();
+
         _shipMotor = GetComponent<ShipMotor>();
     }
+    private void Start()
+    {
+        //photonView.Owner.GetPlayerNumber();
+    }
+    public void Update()
+    {
+        if (!_photonView.IsMine)
+        {
+            return;
+        }
 
-    public void MoveToInput(Vector2 input) {
+        //rotation = Input.GetAxis("Horizontal");
+        //acceleration = Input.GetAxis("Vertical");
+
+        //if (Input.GetButton("Jump") && shootingTimer <= 0.0)
+        //{
+        //    shootingTimer = 0.2f;
+
+        //    photonView.RPC("Fire", RpcTarget.AllViaServer, rigidbody.position, rigidbody.rotation);
+        //}
+
+        //if (shootingTimer > 0.0f)
+        //{
+        //    shootingTimer -= Time.deltaTime;
+        //}
+    }
+    public void FixedUpdate()
+    {
+        if (!_photonView.IsMine)
+        {
+            return;
+        }
+
+        //Quaternion rot = rigidbody.rotation * Quaternion.Euler(0, rotation * RotationSpeed * Time.fixedDeltaTime, 0);
+        //rigidbody.MoveRotation(rot);
+
+        //Vector3 force = (rot * Vector3.forward) * acceleration * 1000.0f * MovementSpeed * Time.fixedDeltaTime;
+        //rigidbody.AddForce(force);
+
+        //if (rigidbody.velocity.magnitude > (MaxSpeed * 1000.0f))
+        //{
+        //    rigidbody.velocity = rigidbody.velocity.normalized * MaxSpeed * 1000.0f;
+        //}
+
+        //CheckExitScreen();
+    }
+    #region COROUTINES
+
+    //private IEnumerator WaitForRespawn()
+    //{
+    //    yield return new WaitForSeconds(AsteroidsGame.PLAYER_RESPAWN_TIME);
+
+    //    _photonView.RPC("RespawnSpaceship", RpcTarget.AllViaServer);
+    //}
+
+    #endregion
+    #region PUN CALLBACKS
+
+    [PunRPC]
+    public void DestroySpaceship()
+    {
+        if (_photonView.IsMine)
+        {
+            // GO TO LOBBY
+            StartCoroutine("WaitForRespawn");
+        }
+    }
+
+    [PunRPC]
+    public void Fire(Vector3 position, Quaternion rotation, PhotonMessageInfo info)
+    {
+        float lag = (float)(PhotonNetwork.Time - info.SentServerTime);
+        GameObject bullet;
+
+        ///** Use this if you want to fire one bullet at a time **/
+        //bullet = Instantiate(BulletPrefab, rigidbody.position, Quaternion.identity) as GameObject;
+        //bullet.GetComponent<Bullet>().InitializeBullet(photonView.Owner, (rotation * Vector3.forward), Mathf.Abs(lag));
+
+
+        /** Use this if you want to fire two bullets at once **/
+        //Vector3 baseX = rotation * Vector3.right;
+        //Vector3 baseZ = rotation * Vector3.forward;
+
+        //Vector3 offsetLeft = -1.5f * baseX - 0.5f * baseZ;
+        //Vector3 offsetRight = 1.5f * baseX - 0.5f * baseZ;
+
+        //bullet = Instantiate(BulletPrefab, rigidbody.position + offsetLeft, Quaternion.identity) as GameObject;
+        //bullet.GetComponent<Bullet>().InitializeBullet(photonView.Owner, baseZ, Mathf.Abs(lag));
+        //bullet = Instantiate(BulletPrefab, rigidbody.position + offsetRight, Quaternion.identity) as GameObject;
+        //bullet.GetComponent<Bullet>().InitializeBullet(photonView.Owner, baseZ, Mathf.Abs(lag));
+    }
+
+    [PunRPC]
+    public void RespawnSpaceship()
+    {
+
+    }
+
+    #endregion
+    public void MoveToInput(Vector2 input)
+    {
         _shipMotor.MoveToInput(input);
     }
 
