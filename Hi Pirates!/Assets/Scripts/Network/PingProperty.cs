fcbbc2e7c75b8bfa05abc8ca6cbae6b2443594f0
gkaragoz <gkaragoz96@gmail.com>
2019-03-22 21:27:14 +0300

    Create PingProperty.cs

---
+++ Hi Pirates!/Assets/Scripts/Network/PingProperty.cs
@@ -0,0 +1,26 @@
+﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class PingProperty : MonoBehaviour {
+
+    [SerializeField]
+    private TextMeshProUGUI _txtPing;
+
+    private int _cache = -1;
+
+    private void Update() {
+        if (PhotonNetwork.IsConnectedAndReady) {
+            if (PhotonNetwork.GetPing() != _cache) {
+                _cache = PhotonNetwork.GetPing();
+                _txtPing.text = _cache.ToString() + " ms";
+            }
+        } else {
+            if (_cache != -1) {
+                _cache = -1;
+                _txtPing.text = "n/a";
+            }
+        }
+    }
+
+}
