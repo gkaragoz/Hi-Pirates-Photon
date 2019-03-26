75775d2df61a943a8c26573737f0d7efd026eaaf
gkaragoz <gkaragoz96@gmail.com>
2019-03-26 14:14:39 +0300

    Create CannonProjectile.cs DestroyTimer.cs and implement Water Splash Particle FX.

---
+++ Hi Pirates!/Assets/Scripts/CannonProjectile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CannonProjectile : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _waterSplashFX;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Water") {
+            Instantiate(_waterSplashFX, new Vector3(transform.position.x, 0f, transform.position.z), Quaternion.identity);
+        }    
+    }
+
+}
---
+++ Hi Pirates!/Assets/Scripts/DestroyTimer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class DestroyTimer : MonoBehaviour {
+
+    private void Awake() {
+        Destroy(this.gameObject, GetComponent<ParticleSystem>().main.duration);
+    }
+
+}
